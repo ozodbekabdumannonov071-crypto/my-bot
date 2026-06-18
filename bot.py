f770be2490a5dfe5ed82955732090c3a50285e66
ozodbekabdumannonov071-crypto <ozodbekabdumannonov071@gmail.com>
2026-06-18 21:57:27 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,1 +1,19 @@
+from flask import Flask
+from threading import Thread
+import telebot # yoki siz qaysi kutubxonani ishlatsangiz
 
+app = Flask('')
+
+@app.route('/')
+def home():
+    return "Bot is running"
+
+def run():
+    app.run(host='0.0.0.0', port=8080)
+
+t = Thread(target=run)
+t.start()
+
+# --- Bu yerdan pastga o'zingizning bot kodingizni yozing ---
+bot = telebot.TeleBot("SIZNING_TOKENINGIZ")
+# ... va qolgan qismi ...
